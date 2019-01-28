24cbd18fa1391a15039ac7386fb1d280a8ee5c9b
Mike Waldron <mikewaldron@helixmail.com>
2019-01-27 17:46:13 -0700

    Rethinking and prepping for multiple threads

--- speedtest.c
+++ speedtest.c
@@ -3,64 +3,103 @@
 #include <unistd.h>
 #include <stdio.h> 
 
-#define A 5000000
-#define B 100000
-#define C 100
-// const int A = 500000000; // How many doubles we're going to pre-generate
-// const int B = 100000; // The value to divide randomly-generated ints...by...to produce doubles
+#define DIVIDENDS 500
+#define DIVISORS 50
+#define THREADS 2
 
-double operand1[A];
-double operand2[A];
+double dividends[DIVIDENDS];
+double divisors[DIVISORS];
+
+pthread_t threadIDs[THREADS];
 
 int stop = 0;
 
-void *thread1(void *vargp) 
+//-------------------------------------------------------------------------------------------------
+// Generate a large-ish, random-ish double value
+//-------------------------------------------------------------------------------------------------
+double getBigDouble() {
+  uint64_t rand1 = rand();
+  uint64_t rand2 = rand();
+  uint64_t bigRand = rand1 * rand2;
+  double bigDouble = bigRand / 1e6;
+  
+  return bigDouble;
+}
+
+//-------------------------------------------------------------------------------------------------
+// Pre-populate the dividend and divisor arrays
+//-------------------------------------------------------------------------------------------------
+void prePopulate() {
+  int i = 0;
+
+  printf("Generating dividends...\n");
+  for (i = 0; i < DIVIDENDS; i++)
+    dividends[i] = getBigDouble();
+
+  printf("Generating divisors...\n");
+  for (i = 0; i < DIVISORS; i++)
+    divisors[i] = getBigDouble();
+}
+
+void *thread(void *vargp) 
 {
-  double d;
+  uint64_t counter = 0;
+  double result = 0.0;
   int i = 0;
   int j = 0;
 
-  while (j < C) {
-    for (i = 0; i < A; i++) {
-      d = operand1[i] * operand2[i];
-      // d = operand1[i] / operand2[i];
+  while (stop == 0) {
+    for (i = 0; i < DIVISORS; i++) {
+      for (j = 0; j < DIVIDENDS; j++) {
+        result = dividends[j] / divisors[i];
+        // printf("%d\t%d\t%f\n", i, j, result);
+        counter++;
+
+        if (stop == 1)
+          break;
+      }
+
       if (stop == 1)
         break;
     }
 
-    if (stop == 1)
-      break;
-    j++;
+    stop = 1;
   }
 
-  if (stop == 0)
-    printf("Finished! (%d)\n", A);
-  else
-    printf("%d\t%d\t%d\n", j, i, A*C);
+  printf("Did %llu divisions\n", counter);
 
   return NULL;
 } 
    
-int main() { 
+int main() {
   srand(getpid());
 
-  // printf("Pre-generating\n");
-  for (int i = 0; i < A; i++) {
-    operand1[i] = rand() / (double)B; // arc4random() / (double)B;
-    operand2[i] = rand() / (double)B; // arc4random() / (double)B;
-  }
-  // printf("Here we go!!\n");
+  prePopulate();
+  thread(NULL);
 
-  // printf("Ready\n");
-  // for (int i = A -1; i > A-50; i--)
-  //   printf("  %f\n", operand1[i]);
+  return 0;
+}
 
-  pthread_t thread_id1; 
-  pthread_create(&thread_id1, NULL, thread1, NULL);
-  sleep(1);
-  stop = 1; 
-  // printf("Time!!\n");
-  pthread_join(thread_id1, NULL); 
+int smain() { 
+  // srand(getpid());
 
-  exit(0); 
+  // // printf("Pre-generating\n");
+  // for (int i = 0; i < A; i++) {
+  //   operand1[i] = rand() / (double)B; // arc4random() / (double)B;
+  //   operand2[i] = rand() / (double)B; // arc4random() / (double)B;
+  // }
+  // // printf("Here we go!!\n");
+
+  // // printf("Ready\n");
+  // // for (int i = A -1; i > A-50; i--)
+  // //   printf("  %f\n", operand1[i]);
+
+  // pthread_t thread_id1; 
+  // pthread_create(&thread_id1, NULL, thread, NULL);
+  // sleep(1);
+  // stop = 1; 
+  // // printf("Time!!\n");
+  // pthread_join(thread_id1, NULL); 
+
+  return(0); 
 }
